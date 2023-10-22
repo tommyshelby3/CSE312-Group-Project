2edef23b5cd6e3560afff4235d43c226ec51c53d
cameronjtoy <cameron.j.toy@gmail.com>
2023-10-21 20:04:27 -0400

    register and login

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import secrets
 from pymongo import MongoClient
 import database as db
+import bcrypt
 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -12,43 +12,43 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/api/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    data = request.get_json()
-    if not data or 'credential' not in data:
-        return jsonify({'error': 'email is required'}), 400
-    credential = data['credential']
-    # Check if the email exists in the database
-    user = db.client_users.find_one({
-        '$or': [
-            {'email': credential},
-            {'username': credential}
-        ]
-    })
-    # If the user is not found in the database
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-    else:
-        # Generate a JWT token for the user and return it
-        token = secrets.token_hex(16)
-        return jsonify({'token': token}), 200  # Added 200 response code here
-
-@app.route('/api/register', methods=['POST'])
-def register():
-    data = request.get_json()
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        data = request.form 
+        username = data['username']
+        password = data['password']
+        user = db.client_users.find_one({'username':username})
+        if user is None:
+            return jsonify({'error' : 'invalid username'}), 400
+        if bcrypt.hashpw(password.encode('utf-8'), user['salt']) == user['password']:
+            auth = secrets.token_hex(16)
+            db.client_users["auth"] = auth
+            return redirect(url_for('index'))
+        else:
+            return jsonify({'error' : 'invalid password'}), 400
     
-    email = data['email']
-    existUser = db.client_users.find_one({'email': email})
-    if existUser:
-        return jsonify({'error' : 'existing email in use'}), 400
-    username = data.get('username')
-    password = data.get('password')  
-    new_user = {
-        'email': email,
-        'username': username,
-        'password': password
-    }
-    db.client_users.insert_one(new_user)
-    token = secrets.token_hex(16)
-    return jsonify({'token': token}), 200 
 
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        data = request.form
+        username = data['username']
+        password = data['password']
+        if username is None or password is None:
+            return jsonify({'error' : 'missing username or password'}), 400
+        salt = bcrypt.gensalt()
+        hashpassword = bcrypt.hashpw(password.encode('utf-8'), salt)
+        db.register_user(username, hashpassword, salt)
+        return jsonify({'success' : 'user created'}), 200
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
--- database.py
+++ database.py
@@ -2,5 +2,32 @@ from pymongo import MongoClient
 import secrets
 import bcrypt
 
-client = MongoClient('localhost', 27017)
-db = client['CSE312-Group-Project']
+client = MongoClient("mongo")
+database = client['CSE312-Group-Project']
+client_users = database['users']
+client_posts = database['posts']
+client_comments = database['comments']
+client_likes = database['likes']
+client_id = database['id']
+
+def get_next_id():
+    idNumber = client_id.find_one({})
+    nextID = 0
+    if idNumber:
+        nextId =int(idNumber['last_id']) + 1
+        client_id.update_one({}, {"$set": {'last_id':nextId}})
+        return nextId
+    else:
+        client_id.insert_one({'last_id':1})
+        return 1
+
+def register_user(username, password, salt):
+    new_user = {
+        'username': username,
+        'password': password,
+        'salt': salt,
+        'auth': None,
+        '_id': get_next_id()
+    }
+    client_users.insert_one(new_user)
+
