fcd2730377ddf1cba5fff35fe99b1bcebcf763d1
cameronjtoy <cameron.j.toy@gmail.com>
2023-10-22 23:55:03 -0400

    wokring button

--- static/functions.js
+++ static/functions.js
@@ -14,21 +14,16 @@ function fetchPosts() {
             postsHtml += `<p>By: ${post.username}</p>`;
             postsHtml += `<p>${post.description}</p>`;
             postsHtml += `<p>Likes: ${post.likes}</p>`;
-            // Adding a "Like" button with a data attribute for the post's unique identifier
             postsHtml += `<button class="like-button" data-post-id="${post._id}">Like</button>`;
             postsHtml += '<hr>';
         });
         $('#postsContainer').html(postsHtml);
-
-        // After the HTML is generated and added to the DOM, add click event listeners to the like buttons.
         addLikeEventListeners();
     });
 }
 
 function task() {
-    fetchPosts();                       // Fetch all posts on page load
-
-    
+    fetchPosts();// Fetch all posts on page load
     setInterval(fetchPosts, 5000)
     $('#createPostForm').submit(function (e) {
         e.preventDefault();
